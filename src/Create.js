67b71ce9623dd6512fbc94985ee2681cc3a867ac
Idan Golshan <idang9@gmail.com>
2021-09-23 11:46:02 +0300

    Added Create component

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,46 @@
+import {useState} from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const blog = {title,body,author}
+        console.log(blog)
+    }
+
     return (
         <div className="create">
             <h2> Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label> Blog Title: </label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e)=>{
+                        setTitle(e.target.value)
+                    }}
+                />
+                <label> Blog body: </label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => {
+                        setBody(e.target.value)}}
+                >
+                </textarea>
+                <label>Blog author: </label>
+                <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="Mario">Mario</option>
+                    <option value="Luigi">Luigi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
     );
+    return title, body, author;
 }
-
 export default Create;
